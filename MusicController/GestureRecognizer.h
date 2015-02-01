65aa7bcfb18e129513b16fa478d976519e5399d3
Martino Giacchetti <martino.giacchetti@gmail.com>
2015-02-01 18:49:20 +0100

    little comment

--- MusicController/GestureRecognizer.h
+++ MusicController/GestureRecognizer.h
@@ -6,6 +6,9 @@
 //  Copyright (c) 2015 Leonardo Lanzinger. All rights reserved.
 //
 
+
+//maybe there is to include the framework
+
 #import <Foundation/Foundation.h>
 #import <CoreMotion/CoreMotion.h>
 
