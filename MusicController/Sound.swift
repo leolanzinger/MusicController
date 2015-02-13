072f787d20189c6f78a2b6864542346aaba15299
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2015-02-13 10:05:40 +0100

    Revert "deleted some files"
    This reverts commit b9696509bdf611e01556ad656d198241360f322f.

---
+++ MusicController/Sound.swift
@@ -0,0 +1,69 @@
+//
+//  Sound.swift
+//  MusicController
+//
+//  Created by Leonardo Lanzinger on 28/01/15.
+//  Copyright (c) 2015 Leonardo Lanzinger. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class Sound : NSObject {
+    
+    /// The player.
+    var avPlayer:AVAudioPlayer!
+    
+    /**
+    Uses AvAudioPlayer to play a sound file.
+    The player instance needs to be an instance variable. Otherwise it will disappear before playing.
+    */
+    func readFileIntoAVPlayer() {
+        
+        var error: NSError?
+        let fileURL:NSURL = NSBundle.mainBundle().URLForResource("1-eclipse", withExtension: "mp3")!
+        
+        // the player must be a field. Otherwise it will be released before playing starts.
+        self.avPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: &error)
+        if avPlayer == nil {
+            if let e = error {
+                println(e.localizedDescription)
+            }
+        }
+        
+        println("playing \(fileURL)")
+        avPlayer.delegate = self
+        avPlayer.prepareToPlay()
+        avPlayer.volume = 1.0
+        avPlayer.play()
+    }
+    
+    func stopAVPLayer() {
+        if avPlayer.playing {
+            avPlayer.stop()
+        }
+    }
+    
+    func toggleAVPlayer() -> Bool {
+        if avPlayer.playing {
+            avPlayer.pause()
+            return false
+        } else {
+            avPlayer.play()
+            return true
+        }
+    }
+}
+
+// MARK: AVAudioPlayerDelegate
+extension Sound : AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+        println("finished playing \(flag)")
+    }
+    
+    
+    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer!, error: NSError!) {
+        println("\(error.localizedDescription)")
+    }
+    
+}
