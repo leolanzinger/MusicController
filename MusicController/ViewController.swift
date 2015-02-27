db7493df3e58d4095f3441ea168eb21d42a496b7
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2015-02-27 15:01:41 +0100

    music playback working

--- MusicController/ViewController.swift
+++ MusicController/ViewController.swift
@@ -7,23 +7,34 @@
 //
 
 import UIKit
-import MediaPlayer
 
 class ViewController: UIViewController {
 
+    // buttons
     @IBOutlet weak var playButton: UIButton!
-    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var volumeUpButton: UIButton!
+    @IBOutlet weak var volumeDownButton: UIButton!
     
-    var mediaPlayer: MPMoviePlayerController = MPMoviePlayerController()
+    // labels
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var volumeLabel: UILabel!
+    
     var playing = false
     
+    // initialize classes for sound playing and gestures recognition
     var gestureRecognizer: GestureRecognizer = GestureRecognizer()
-    
     let audioPlayer: Sound = Sound()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // start audio player
         audioPlayer.readFileIntoAVPlayer()
+        
+        // get current volume level
+        volumeLabel.text = "\(audioPlayer.getVolume())"
 
         var rdfParser: RdfParser = RdfParser()
         // Do not parse rdf for now
@@ -35,15 +46,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /**
+    IBActions to control music playback
+    */
     @IBAction func playStopMusic(sender: AnyObject) {
         if (playing) {
             playing = false
             playButton.setTitle("Play", forState: UIControlState.Normal)
+            audioPlayer.toggleAVPlayer()
         }
         else {
             playing = true
             playButton.setTitle("Pause", forState: UIControlState.Normal)
+            audioPlayer.toggleAVPlayer()
         }
+    }
+    
+    @IBAction func nextSong(sender: AnyObject) {
+        audioPlayer.nextSong()
+    }
+    
+    @IBAction func previousSong(sender: AnyObject) {
+        audioPlayer.previousSong()
+    }
+    
+    @IBAction func volumeUp(sender: AnyObject) {
+        audioPlayer.volumeUp()
+        volumeLabel.text = "\(audioPlayer.getVolume())"
+    }
+    
+    @IBAction func volumeDown(sender: AnyObject) {
+        audioPlayer.volumeDown()
+        volumeLabel.text = "\(audioPlayer.getVolume())"
     }
 
 }
